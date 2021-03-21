5d5cc3e6bed4e69fdbbd6bd396680789d0f68acd
Mida Adawiyah <midaadawiyah0927@gmail.com>
2021-03-21 08:34:21 +0700

    Add files via upload

---
+++ Biodata Singkat.pas
@@ -0,0 +1,24 @@
+Program biodata;
+uses crt;
+
+Var
+nama,alamat,ttl,nohp,motto : string;
+npm : integer;
+
+begin
+clrscr;
+writeln ('PROGRAM BIODATA');
+write ('Masukkan nama :');
+readln (nama)                                      ;
+write ('Masukkan npm :');
+readln (npm);
+write ('Masukkan alamat :');
+readln (Alamat);
+write ('Masukkan ttl :');
+readln(ttl);
+write ('Masukkan nohp :');
+readln (nohp);
+write ('Masukkan motto :');
+readln (motto);
+end.
+
