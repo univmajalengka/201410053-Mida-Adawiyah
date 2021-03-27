5c385ba9a8db10ac2b608fe0cfb9fb0df42d4513
Mida Adawiyah <midaadawiyah0927@gmail.com>
2021-03-27 19:06:18 +0700

    Tugas 2

---
+++ Tugas 2/Konversi suhu fahrenheit ke celcius.pas
@@ -0,0 +1,16 @@
+program konversi suhu fahrenheit ke celcius;
+uses crt;
+
+var
+i,f:real;
+
+begin
+clrscr;
+writeln ('Program konversi suhu fahrenhei ke celcius');
+writeln ('------------------------------------------');
+write   ('Masukkan suhu dalam fahrenheit;'); readln(I);
+f:= (5/9)*(i-32);
+writeln ('suhu ',i:0:2' Fahrenheit dalam celcius adalah',f:0:2);
+readln;
+
+end.
